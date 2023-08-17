67831274f8a75439357c1bbb1d30c12d0fb57b27
Mohd Mazlan Bin Mohd Nor <lan.psis@gmail.com>
2023-08-17 10:37:07 +0800

    update code

--- dist/index.js
+++ dist/index.js
@@ -1,47 +1,59 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
-const generate_1 = require("./commands/generate");
 const readline_1 = __importDefault(require("readline"));
-const figlet = require("figlet");
-console.log(figlet.textSync("Spring Generator"));
-const program = new commander_1.Command();
-program
-    .version('1.0.0')
-    .description('A CLI tool to generate Spring Boot Files')
-    .option('-r, --route <routePath>', 'Specify route path')
-    .option('-c, --controller <controllerName>', 'Specify controller name')
-    .parse(process.argv);
-const options = program.opts();
+const figlet_1 = __importDefault(require("figlet"));
+const generate_1 = require("./commands/generate");
 const rl = readline_1.default.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question('Enter controller name: ', (controllerName) => {
-    rl.question('Enter module name: ', (moduleName) => {
-        rl.question('Enter entity name: ', (entityName) => {
-            rl.question('Enter service SO name: ', (serviceSO) => {
-                rl.question('Enter route path: ', (routePath) => {
-                    rl.close();
-                    const moduleNameLowerCase = moduleName.toLowerCase();
-                    const routePathLowerCase = routePath.toLowerCase();
-                    const options = {
-                        moduleNameLowerCase,
-                        modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
-                        entityNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`,
-                        serviceSOPascalCase: serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1) + 'SO',
-                        serviceSOCamelCase: serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1) + 'SO',
-                        routePathLowerCase,
-                        controllerNamePascalCase: controllerName.charAt(0).toUpperCase() + controllerName.slice(1) + 'Controller'
-                    };
-                    (0, generate_1.generateSpringBootController)(options);
-                });
-            });
-        });
+const promptUser = (question) => new Promise((resolve) => {
+    rl.question(question, (answer) => {
+        resolve(answer);
     });
 });
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(figlet_1.default.textSync('Spring Generator'));
+    const program = new commander_1.Command();
+    program
+        .version('1.0.0')
+        .description('A CLI tool to generate Spring Boot Files')
+        .option('-r, --route <routePath>', 'Specify route path')
+        .option('-c, --controller <controllerName>', 'Specify controller name')
+        .parse(process.argv);
+    const options = program.opts();
+    const controllerName = options.controller || (yield promptUser('Enter controller name: '));
+    const moduleName = yield promptUser('Enter module name (ex: user): ');
+    const entityName = yield promptUser('Enter entity name (ex: user [without Entity keyword]): ');
+    const serviceSO = yield promptUser('Enter service SO name (ex: user [without SO keyword]): ');
+    const routePath = options.route || (yield promptUser('Enter route path (ex: user [endpoint path]): '));
+    const moduleNameLowerCase = moduleName.toLowerCase();
+    const routePathLowerCase = routePath.toLowerCase();
+    const params = {
+        moduleNameLowerCase,
+        routePathLowerCase,
+        modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
+        entityNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`,
+        serviceSOPascalCase: serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1) + 'SO',
+        serviceSOCamelCase: serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1) + 'SO',
+        controllerNamePascalCase: controllerName.charAt(0).toUpperCase() + controllerName.slice(1) + 'Controller'
+    };
+    (0, generate_1.generateSpringBootController)(params);
+    rl.close();
+});
+run();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,56 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-import { Command  } from 'commander';
-import { ControllerData, generateSpringBootController } from './commands/generate';
+import { Command } from 'commander';
 import readline from 'readline';
-const figlet = require("figlet");
-
-console.log(figlet.textSync("Spring Generator"));
-const program = new Command();
-program
-    .version('1.0.0')
-    .description('A CLI tool to generate Spring Boot Files')
-    .option('-r, --route <routePath>', 'Specify route path')
-    .option('-c, --controller <controllerName>', 'Specify controller name')
-    .parse(process.argv);
-
-const options = program.opts();
+import figlet from 'figlet';
+import { generateSpringBootController, ControllerData } from './commands/generate';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Enter controller name: ', (controllerName) => {
-    rl.question('Enter module name: ', (moduleName) => {
-        rl.question('Enter entity name: ', (entityName) => {
-            rl.question('Enter service SO name: ', (serviceSO) => {
-                rl.question('Enter route path: ', (routePath) => {
-                    rl.close();
-
-                    const moduleNameLowerCase = moduleName.toLowerCase();
-                    const routePathLowerCase = routePath.toLowerCase();
-
-                    const options: ControllerData = {
-                        moduleNameLowerCase,
-                        modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
-                        entityNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`,
-                        serviceSOPascalCase: serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1) + 'SO',
-                        serviceSOCamelCase: serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1) + 'SO',
-                        routePathLowerCase,
-                        controllerNamePascalCase: controllerName.charAt(0).toUpperCase() + controllerName.slice(1) + 'Controller'
-                    };
-
-                    generateSpringBootController(options);
-                });
-            });
-        });
+const promptUser = (question: string): Promise<string> => new Promise((resolve) => {
+    rl.question(question, (answer) => {
+        resolve(answer);
     });
 });
 
+const run = async () => {
+    console.log(figlet.textSync('Spring Generator'));
 
+    const program = new Command();
+    program
+        .version('1.0.0')
+        .description('A CLI tool to generate Spring Boot Files')
+        .option('-r, --route <routePath>', 'Specify route path')
+        .option('-c, --controller <controllerName>', 'Specify controller name')
+        .parse(process.argv);
+
+    const options = program.opts();
+
+    const controllerName = options.controller || await promptUser('Enter controller name: ');
+    const moduleName = await promptUser('Enter module name (ex: user): ');
+    const entityName = await promptUser('Enter entity name (ex: user [without Entity keyword]): ');
+    const serviceSO = await promptUser('Enter service SO name (ex: user [without SO keyword]): ');
+    const routePath = options.route || await promptUser('Enter route path (ex: user [endpoint path]): ');
+
+    const moduleNameLowerCase = moduleName.toLowerCase();
+    const routePathLowerCase = routePath.toLowerCase();
+
+    const params: ControllerData = {
+        moduleNameLowerCase,
+        routePathLowerCase,
+        modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
+        entityNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`,
+        serviceSOPascalCase: serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1) + 'SO',
+        serviceSOCamelCase: serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1) + 'SO',
+        controllerNamePascalCase: controllerName.charAt(0).toUpperCase() + controllerName.slice(1) + 'Controller'
+    };
+
+    generateSpringBootController(params);
+
+    rl.close();
+};
+
+run();
