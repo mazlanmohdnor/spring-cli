b0c93155d937fa5a3bb96209159cca9ae88f14bd
Mohd Mazlan Bin Mohd Nor <lan.psis@gmail.com>
2023-08-21 17:49:20 +0800

    added mapper and repo

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -2,49 +2,138 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const templatePath = path.join(__dirname, 'templates', 'ControllerTemplate.txt');
+const repositoryPath = path.join(__dirname, 'templates', 'RepositoryTemplate.txt');
+const mapperPath = path.join(__dirname, 'templates', 'MapperTemplate.txt');
 const ServiceSOPath = path.join(__dirname, 'templates', 'ServiceSO.txt');
-const ServiceSOImplePath = path.join(__dirname, 'templates', 'ServiceSOImple.txt');
+const ServiceSOImplPath = path.join(__dirname, 'templates', 'ServiceSOImpl.txt');
 
 export interface ControllerData {
     moduleNameLowerCase: string;
     modelNamePascalCase: string;
     entityNamePascalCase: string;
-    serviceSOPascalCase: string;
-    serviceSOCamelCase: string;
+    serviceSOPascalCase?: string;
+    serviceSOCamelCase?: string;
     routePathLowerCase: string;
     controllerNamePascalCase: string;
     repositoryNamePascalCase: string;
     repositoryNameCamelCase: string;
-    [key: string]: string; // Allow dynamic property access
+    // [key: string]: string; // Allow dynamic property access
 }
 
 export function generateController(data: ControllerData): string {
     const template = fs.readFileSync(templatePath, 'utf-8');
+    // @ts-ignore
     return template.replace(/\${(.*?)}/g, (_, match) => data[match.trim()] || '');
 }
 
-export function generateService(data: ControllerData): string {
+export function generateRepository(data: ControllerData): string {
+    const template = fs.readFileSync(repositoryPath, 'utf-8');
+    // @ts-ignore
+    return template.replace(/\${(.*?)}/g, (_, match) => data[match.trim()] || '');
+}
+
+export function generateMapper(data: ControllerData): string {
+    const template = fs.readFileSync(mapperPath, 'utf-8');
+    // @ts-ignore
+    return template.replace(/\${(.*?)}/g, (_, match) => data[match.trim()] || '');
+}
+
+export function generateService(params: ControllerData): string {
+    const directoryPath = `ngos-service/src/main/java/my/com/sapura/ngos/service/application/${params.moduleNameLowerCase}`;
+
     const template = fs.readFileSync(ServiceSOPath, 'utf-8');
-    return template.replace(/\${(.*?)}/g, (_, match) => data[match.trim()] || '');
+    // @ts-ignore
+    return template.replace(/\${(.*?)}/g, (_, match) => params[match.trim()] || '');
 }
 
-export function generateServiceImpl(data: ControllerData): string {
-    const template = fs.readFileSync(ServiceSOImplePath, 'utf-8');
-    return template.replace(/\${(.*?)}/g, (_, match) => data[match.trim()] || '');
+export function generateServiceImpl(params: ControllerData): string {
+
+    const template = fs.readFileSync(ServiceSOImplPath, 'utf-8');
+    // @ts-ignore
+    return template.replace(/\${(.*?)}/g, (_, match) => params[match.trim()] || '');
 }
 
 export function generateSpringBootController(params: ControllerData): void {
+    const directoryPath = `ngos-web/src/main/java/my/com/sapura/ngos/controller/application/${params.moduleNameLowerCase}`;
+
     const generatedCode = generateController(params);
     const fileName = `${params.controllerNamePascalCase}Controller.java`;
-    fs.writeFileSync(fileName, generatedCode, 'utf-8');
+    const filePath = path.join(directoryPath, fileName);
+
+    // Check if the directory exists, and if not, create it
+    if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true }); // Use { recursive: true } to create parent directories if they don't exist
+    }
+
+    try {
+        fs.writeFileSync(filePath, generatedCode, 'utf-8');
+        console.log(`✅ Successfully generated ${filePath}`);
+    } catch (error: any) {
+        console.error(`❌ Error generating ${filePath}: ${error.message}`);
+    }
+}
+
+export function generateSpringBootRepository(params: ControllerData): void {
+    const directoryPath = `ngos-repository/src/main/java/my/com/sapura/ngos/orm/repository/${params.moduleNameLowerCase}`;
+
+    const generatedCode = generateRepository(params);
+    const fileName = `${params.controllerNamePascalCase}Repository.java`;
+    const filePath = path.join(directoryPath, fileName);
+
+    // Check if the directory exists, and if not, create it
+    if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true }); // Use { recursive: true } to create parent directories if they don't exist
+    }
+
+    try {
+        fs.writeFileSync(filePath, generatedCode, 'utf-8');
+        console.log(`✅ Successfully generated ${fileName}`);
+    } catch (error: any) {
+        console.error(`❌ Error generating ${fileName}: ${error.message}`);
+    }
+}
+
+export function generateSpringBootMapper(params: ControllerData): void {
+    const directoryPath = `ngos-mapper/src/main/java/my/com/sapura/ngos/mapper/${params.moduleNameLowerCase}`;
+
+    const generatedCode = generateMapper(params);
+    const fileName = `${params.controllerNamePascalCase}Mapper.java`;
+    const filePath = path.join(directoryPath, fileName);
+
+    // Check if the directory exists, and if not, create it
+    if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true }); // Use { recursive: true } to create parent directories if they don't exist
+    }
+
+    try {
+        fs.writeFileSync(filePath, generatedCode, 'utf-8');
+        console.log(`✅ Successfully generated ${fileName}`);
+    } catch (error: any) {
+        console.error(`❌ Error generating ${fileName}: ${error.message}`);
+    }
 }
 
 export function generateSpringBootService(params: ControllerData): void {
+    const directoryPath = `ngos-service/src/main/java/my/com/sapura/ngos/service/application/${params.moduleNameLowerCase}`;
+
     const generatedService = generateService(params);
     const generatedServiceImpl = generateServiceImpl(params);
     const serviceSO = `${params.controllerNamePascalCase}SO.java`;
     const serviceSOImpl = `${params.controllerNamePascalCase}SOImpl.java`;
+    const serviceSOPath = path.join(directoryPath, serviceSO);
+    const serviceSOImplPath = path.join(directoryPath, serviceSOImpl);
 
-    fs.writeFileSync(serviceSO, generatedService, 'utf-8');
-    fs.writeFileSync(serviceSOImpl, generatedServiceImpl, 'utf-8');
+    // Check if the directory exists, and if not, create it
+    if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true }); // Use { recursive: true } to create parent directories if they don't exist
+    }
+
+    try {
+        fs.writeFileSync(serviceSOPath, generatedService, 'utf-8');
+        fs.writeFileSync(serviceSOImplPath, generatedServiceImpl, 'utf-8');
+        console.log(`✅ Successfully generated ${serviceSO} and ${serviceSOImpl}`);
+    } catch (error: any) {
+        console.error(`❌ Error generating ${serviceSO} and ${serviceSOImpl}: ${error.message}`);
+    }
 }
+
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ import readline from 'readline';
 import figlet from 'figlet';
 import chalk from 'chalk';
 
-import { generateSpringBootController, generateSpringBootService, ControllerData } from './commands/generate';
+import {
+    generateSpringBootController,
+    generateSpringBootService,
+    ControllerData,
+    generateSpringBootRepository,
+    generateSpringBootMapper
+} from './commands/generate';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -19,73 +25,69 @@ const promptUser = (question: string): Promise<string> => new Promise((resolve) 
 });
 
 const run = async () => {
-    console.log(chalk.greenBright(figlet.textSync('NGOS Spring File Generator')));
+    console.log(chalk.greenBright(figlet.textSync('NGOS File Generator')));
 
-    // Display a welcoming message and usage instructions
     console.log(chalk.cyanBright('Welcome to the NGOS Spring File Generator CLI!'));
     console.log(chalk.cyanBright('This tool helps you generate Spring Boot files easily.'));
     console.log(chalk.cyanBright('Usage: spring'));
 
-    const program = new Command();
-    program
-        .version('1.0.1-alpha-5')
+    const program = new Command()
+        .version('1.0.1-alpha-6')
         .description(chalk.blueBright('🌼 A CLI tool to generate Spring Boot Files 🌼'))
         .parse(process.argv);
 
-    const generateChoice = await promptUser(chalk.magentaBright('🚀 What would you like to generate? (c: Controller, s: Service, a: All): '));
+    const generateChoice = await promptUser(chalk.magentaBright('🚀 What would you like to generate? (c: Controller, s: Service, r: Repository, m: Mapper, a: All): '));
+    const moduleName = await promptUser(chalk.yellowBright('📦 Enter module name (ex: user): '));
+    const entityName = await promptUser(chalk.yellowBright('👜 Enter entity name (ex: user [without Entity keyword]): '));
+
+    const moduleNameLowerCase = moduleName.toLowerCase();
+    const entityNamePascalCase = `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`;
+    const controllerNamePascalCase = moduleName.charAt(0).toUpperCase() + moduleName.slice(1);
+    const repositoryNamePascalCase = controllerNamePascalCase;
+    const repositoryNameCamelCase = moduleNameLowerCase;
+
+    const params: ControllerData = {
+        moduleNameLowerCase,
+        routePathLowerCase: '',
+        modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
+        entityNamePascalCase,
+        controllerNamePascalCase,
+        repositoryNamePascalCase,
+        repositoryNameCamelCase
+    };
 
     if (generateChoice.toLowerCase() === 'c' || generateChoice.toLowerCase() === 'a') {
-        const moduleName = await promptUser(chalk.yellowBright('📦 Enter module name (ex: user): '));
-        const routePath = await promptUser(chalk.yellowBright('🌐 Enter route path (ex: user [endpoint path]): '));
-        const entityName = await promptUser(chalk.yellowBright('🏛️ Enter entity name (ex: user [without Entity keyword]): '));
         const serviceSO = await promptUser(chalk.yellowBright('🔧 Enter service SO name (ex: user [without SO keyword]): '));
+        const serviceSOPascalCase = serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1);
+        const serviceSOCamelCase = serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1);
 
-        const moduleNameLowerCase = moduleName.toLowerCase();
-        const routePathLowerCase = routePath.toLowerCase();
+        params.serviceSOPascalCase = serviceSOPascalCase;
+        params.serviceSOCamelCase = serviceSOCamelCase;
 
-        const params: ControllerData = {
-            moduleNameLowerCase,
-            routePathLowerCase,
-            modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
-            entityNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`,
-            serviceSOPascalCase: serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1),
-            serviceSOCamelCase: serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1),
-            controllerNamePascalCase: moduleName.charAt(0).toUpperCase() + moduleName.slice(1),
-            repositoryNamePascalCase: moduleName.charAt(0).toUpperCase() + moduleName.slice(1),
-            repositoryNameCamelCase: moduleName.charAt(0).toLowerCase() + moduleName.slice(1)
-        };
-
+        const routePath = await promptUser(chalk.yellowBright('🌐 Enter route path (ex: user [endpoint path]): '));
+        params.routePathLowerCase = routePath.toLowerCase();
         generateSpringBootController(params);
 
         if (generateChoice.toLowerCase() === 'a') {
             generateSpringBootService(params);
+            generateSpringBootRepository(params);
+            generateSpringBootMapper(params);
         }
-    } else if (generateChoice.toLowerCase() === 's') {
-        const moduleName = await promptUser(chalk.yellowBright('📦 Enter module name (ex: user): '));
-        const entityName = await promptUser(chalk.yellowBright('🏛️ Enter entity name (ex: user [without Entity keyword]): '));
-        const serviceSO = await promptUser(chalk.yellowBright('🔧 Enter service SO name (ex: user [without SO keyword]): '));
-
-        const moduleNameLowerCase = moduleName.toLowerCase();
-
-        const params: ControllerData = {
-            moduleNameLowerCase,
-            routePathLowerCase: '',
-            modelNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Model`,
-            entityNamePascalCase: `${entityName.charAt(0).toUpperCase() + entityName.slice(1)}Entity`,
-            serviceSOPascalCase: serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1),
-            serviceSOCamelCase: serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1),
-            controllerNamePascalCase: moduleName.charAt(0).toUpperCase() + moduleName.slice(1),
-            repositoryNamePascalCase: moduleName.charAt(0).toUpperCase() + moduleName.slice(1),
-            repositoryNameCamelCase: moduleName.charAt(0).toLowerCase() + moduleName.slice(1)
-        };
-
-        generateSpringBootService(params);
-
+    } else if (generateChoice.toLowerCase() === 's' || generateChoice.toLowerCase() === 'r' || generateChoice.toLowerCase() === 'm') {
+        const type = generateChoice.toLowerCase() === 's' ? 'Service' : (generateChoice.toLowerCase() === 'r' ? 'Repository' : 'Mapper');
+        const serviceSO = await promptUser(chalk.yellowBright(`🔧 Enter ${type} name (ex: user [without ${type} keyword]): `));
+        const serviceSOPascalCase = serviceSO.charAt(0).toUpperCase() + serviceSO.slice(1);
+        const serviceSOCamelCase = serviceSO.charAt(0).toLowerCase() + serviceSO.slice(1);
+        params.serviceSOPascalCase = serviceSOPascalCase;
+        params.serviceSOCamelCase = serviceSOCamelCase;
+        generateSpringBootController(params);
     } else {
         console.log(chalk.redBright('❌ Invalid choice.'));
     }
 
     rl.close();
 };
+
+
 
 run();
